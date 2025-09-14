f11cd655ce02f77ecb01ceb62a313951ae273e20
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-09-14 18:34:48 +0200

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useState} from "react";
 
 
 const tracks = [
@@ -28,9 +29,10 @@ const tracks = [
     },
 ]
 
-const selectedTrackId = 1
 
 export function App() {
+
+    const [selectedTrackId, setSelectedTrackId] = useState(null)
 
     if (tracks === null) {
         return (
@@ -54,13 +56,14 @@ export function App() {
     return (
         <>
             <h1>Musicfun player</h1>
+            <button onClick={() => setSelectedTrackId(null)}>Reset selection</button>
             <ul>
                 {tracks.map((track) => (
-                    <li key={track.id}
-                        style={{border: track.id === selectedTrackId ? "1px solid orange" : "none"}}
-                    >
-                        <div onClick={() => alert(track.id)}>{track.title}</div>
-                        <audio controls src={track.url}></audio>
+                    <li key={track.id} style={{border: track.id === selectedTrackId ? '1px solid orange' : 'none'}}>
+                        <div onClick={() => {
+                            setSelectedTrackId(track.id)
+                        }}>{track.title}</div>
+                        <audio src={track.srс} controls={true}/>
                     </li>
                 ))}
             </ul>

5fb70bb0faed9d008d03afbbc1509a645d1365f4
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-09-14 14:01:43 +0200

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {createRoot} from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootEl = document.getElementById("root")
+const reactRoot = createRoot(rootEl!) // 💡 '!' - говорит TypeScript: "этот элемент точно существует".
+reactRoot.render(<App />) // React "оживляет" HTML и вставляет разметку в root.
